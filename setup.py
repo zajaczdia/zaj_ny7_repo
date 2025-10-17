f37cdf3107006e130c67855b6c25bc35ebe2cdf9
zajaczdia <zajaczdiana@gmail.com>
2025-10-17 21:36:07 +0200

    setup1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    #data_files=[
+    data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
